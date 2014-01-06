86cbadfdd9a2162b379f6db4f50ddfb4fb706da4
jungeun826 <jungeun826@nate.com>
2014-01-06 13:46:21 +0900

    실습 여러가지를 모아서 해결했다.
    139페이지, 140페이지에 있는 실습을 완료했다.
    139페이지 :  datePicker를 이용해 날짜 받아서 textField에 출력한다.
    140페이지 : datePicker와 timer를 이용한 실습을 했다.

--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -10,14 +10,55 @@
 
 @interface ViewController ()
 
+/*datePicker 실습 oulet property*/
+@property (weak, nonatomic) IBOutlet UITextField *yearField;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *monthField;
+@property (weak, nonatomic) IBOutlet UITextField *dayField;
+
+/*datePicker와 timer실습 oulet property , 함수*/
+@property (weak, nonatomic) IBOutlet UIDatePicker *timerPicker;
+- (void)stopTimer;
+- (void)startTimer;
+
+/*datePicker와 actionSheet 실습 ouplet property*/
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSTimer *timer;
+    BOOL timerWorking;
+}
+- (IBAction)pickerChanged:(id)sender {
+    NSLog(@"pickerChanged");
+    NSDate *selectedDate = self.datePicker.date;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger flag = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit;
+    NSDateComponents *comp = [calendar components:flag fromDate:selectedDate ];
+    self.yearField.text = [NSString stringWithFormat:@"%ld", (long)[comp year]];
+    self.monthField.text = [NSString stringWithFormat:@"%ld", (long)[comp month]];
+    self.dayField.text = [NSString stringWithFormat:@"%ld", (long)[comp day]];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    /*datePicker 실습 localValue*/
+    self.yearField.enabled = NO;
+    self.monthField.enabled = NO;
+    self.dayField.enabled = NO;
+    
+    /*datePicker와 tiemr 실습 localValue*/
+    timerWorking = NO;
+    
+    /*datePicker와 actionSheet 실습 localValue*/
+    UIActionSheet *sheet;
+    UIDatePicker *picker;
+    NSDateFormatter *formmatter;
+    float height;
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +66,28 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)tickDown:(NSTimer *)timer{
+    self.timerPicker.countDownDuration -=  60;
+    if(self.timerPicker.countDownDuration <= 60){
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+- (void)stopTimer{
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+- (void)startTimer{
+    timer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
+}
+- (IBAction)toggleTimer:(UIButton *)sender{
+    NSString *title = timerWorking ? @"Start":@"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    if( timerWorking)
+        [self startTimer];
+    else
+        [self startTimer];
+}
 @end
