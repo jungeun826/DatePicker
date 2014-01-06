2d42a5e869888dcbf77c793d1c46c48171c6bf90
jungeun826 <jungeun826@nate.com>
2014-01-06 14:02:56 +0900

    실습 143~145를 추가하였다.
    액션시트 안에 DatePicker를 넣는 실습을 하였다.

--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -27,8 +27,15 @@
 @end
 
 @implementation ViewController{
+    /*datePicker와 tiemr 실습 localValue*/
     NSTimer *timer;
     BOOL timerWorking;
+    
+    /*datePicker와 actionSheet 실습 localValue*/
+    UIActionSheet *sheet;
+    UIDatePicker *picker;
+    NSDateFormatter *formatter;
+    float height;
 }
 - (IBAction)pickerChanged:(id)sender {
     NSLog(@"pickerChanged");
@@ -46,19 +53,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    /*datePicker 실습 localValue*/
     self.yearField.enabled = NO;
     self.monthField.enabled = NO;
     self.dayField.enabled = NO;
     
-    /*datePicker와 tiemr 실습 localValue*/
     timerWorking = NO;
-    
-    /*datePicker와 actionSheet 실습 localValue*/
-    UIActionSheet *sheet;
-    UIDatePicker *picker;
-    NSDateFormatter *formmatter;
-    float height;
 }
 
 - (void)didReceiveMemoryWarning
@@ -81,6 +80,8 @@
 - (void)startTimer{
     timer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
 }
+/*timer에 따라 button의 출력값을 바꿔주고, 
+ 취해야 할 acitond을 실행해준다.*/
 - (IBAction)toggleTimer:(UIButton *)sender{
     NSString *title = timerWorking ? @"Start":@"Stop";
     [sender setTitle:title forState:UIControlStateNormal];
@@ -89,5 +90,40 @@
         [self startTimer];
     else
         [self startTimer];
+}
+-(void)handleDone:(id)sender{
+    [sheet dismissWithClickedButtonIndex:0 animated:YES];
+    
+    if(formatter == nil){
+        formatter = [[NSDateFormatter alloc]init];
+        [formatter setDateFormat:@"yyyy년 MM월 dd일"];
+    }
+    NSDate *date = picker.date;
+    NSString *dateStr = [formatter stringFromDate:date];
+    [self.button setTitle:dateStr forState:UIControlStateNormal];
+}
+- (IBAction)chooseDate:(id)sender{
+    CGSize viewSize = self.view.bounds.size;
+    if(sheet == nil){
+        sheet = [[UIActionSheet alloc]init];
+        
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0,0,viewSize.width,44)];
+        UIBarButtonItem *done = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(handleDone:)];
+        NSArray *items = [NSArray arrayWithObjects:done, nil];
+        [toolbar setItems:items];
+        picker = [[UIDatePicker alloc]init];
+        picker.datePickerMode = UIDatePickerModeDateAndTime;
+        picker.frame = CGRectMake(0,toolbar.frame.size.height, viewSize.width, picker.frame.size.height);
+        
+        [sheet addSubview:toolbar];
+        [sheet addSubview:picker];
+        
+        [sheet showInView:self.view];
+        
+        height = toolbar.frame.size.height + picker.frame.size.height;
+    }
+    
+    [sheet showInView:self.view];
+    sheet.frame = CGRectMake(0, viewSize.height - height, viewSize.width, height);
 }
 @end
